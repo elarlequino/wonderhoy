6e4abe442f4f5d40d4b3d3a5e3c33779688955b0
elarlequino <maxlefebvre2005@gmail.com>
2025-07-22 14:22:03 +1100

    gsap
    ajout gsap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+navigator.clipboard
+  .writeText(email)
+  .then(() => alert("Email copié dans le presse-papiers !"))
+  .catch(() => alert("Erreur lors de la copie."));
+import gsap from "gsap";
+
 function copyEmail() {
   const email = "maxlefebvre2005@gmail.com"; // Remplace par ton email
-  navigator.clipboard
-    .writeText(email)
-    .then(() => alert("Email copié dans le presse-papiers !"))
-    .catch(() => alert("Erreur lors de la copie."));
 }
